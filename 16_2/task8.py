db19c17bbbeef372285eb85e02c03eac65284651
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2026-03-05 12:43:49 +0300

    Implement batch gradient descent update function
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

---
+++ 16_2/task8.py
@@ -0,0 +1,28 @@
+def update_batch(xs, ys, w, b, lr):
+    m = len(xs)
+    sw = 0.0
+    sb = 0.0
+    for x, y in zip(xs, ys):
+        e = (w * x + b) - y
+        sw += e * x
+        sb += e
+    w -= lr * (2.0 * sw / m)
+    b -= lr * (2.0 * sb / m)
+    return w, b
+
+n, batch_size = map(int, input().split())
+xs = []
+ys = []
+for _ in range(n):
+    x, y = map(float, input().split())
+    xs.append(x)
+    ys.append(y)
+w, b, lr = map(float, input().split())
+
+i = 0
+while i < n:
+    j = min(i + batch_size, n)
+    w, b = update_batch(xs[i:j], ys[i:j], w, b, lr)
+    i = j
+
+print('{:.6f} {:.6f}'.format(w, b))
